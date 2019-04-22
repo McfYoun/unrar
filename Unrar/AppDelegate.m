8dd4a05da8409818529bb1c29f215bab5a4a6475
Delevin.Bai <delevin.bai@b-padynamics.com>
2019-04-22 10:10:31 +0800

    Initial Commit

---
+++ Unrar/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  Unrar
+//
+//  Created by BP on 22/04/2019.
+//  Copyright © 2019 BP. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ Unrar/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Unrar
+//
+//  Created by BP on 22/04/2019.
+//  Copyright © 2019 BP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
